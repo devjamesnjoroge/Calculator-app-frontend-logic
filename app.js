cd90623a08d3eb9e63a38b3e5f86e91a90908429
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-03-02 05:50:31 +0300

    fix: reduced button gaps in longer devices

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-var nine = document.querySelector
+var nine = document.querySelector("#nine");
+var eight = document.querySelector("#eight");
+var seven = document.querySelector("#seven");
+var six = document.querySelector("#six");
+var five = document.querySelector("#five");
+var four = document.querySelector("#four");
+var three = document.querySelector("#three");
+var two = document.querySelector("#two");
+var one = document.querySelector("#one");
+var zero = document.querySelector("#zero");
+var ans = document.querySelector("#ans");
+var reset = document.querySelector("#reset");
+var dec = document.querySelector("#decimal");
+var multiply = document.querySelector("#multiply");
+var divide = document.querySelector("#divide");
+var plus = document.querySelector("#plus");
+var minus = document.querySelector("#minus");
+
+var numbersArray = [zero, one, two, three, four, five, six, seven, eight, nine];
+var operatorsArray = [plus, minus, multiply, divide];
+var mainBtns = [reset, ans];
