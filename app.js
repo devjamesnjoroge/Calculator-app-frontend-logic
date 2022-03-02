bcc9ff3b8993191ad8f4afc2d51ce780ff226818
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-03-02 07:39:59 +0300

    ft: add UI logic using js appending values

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// USER INTERFACE LOGIC
+
 // get calculator buttons DOM elements
 
 var numbersArray = [
@@ -26,7 +28,6 @@ var mainBtns = [
 ];
 
 var output = document.getElementById("output");
-var outputVal = document.getElementById("output").value;
 
 // add click event listener to initialize functions
 
@@ -34,6 +35,63 @@ numbersArray[0].addEventListener("click", function (event) {
   output.append(0);
 });
 
+numbersArray[1].addEventListener("click", function (event) {
+  output.append(1);
+});
+numbersArray[2].addEventListener("click", function (event) {
+  output.append(2);
+});
+numbersArray[3].addEventListener("click", function (event) {
+  output.append(3);
+});
+numbersArray[4].addEventListener("click", function (event) {
+  output.append(4);
+});
+numbersArray[5].addEventListener("click", function (event) {
+  output.append(5);
+});
+numbersArray[6].addEventListener("click", function (event) {
+  output.append(6);
+});
+numbersArray[7].addEventListener("click", function (event) {
+  output.append(7);
+});
+numbersArray[8].addEventListener("click", function (event) {
+  output.append(8);
+});
+numbersArray[9].addEventListener("click", function (event) {
+  output.append(9);
+});
+numbersArray[10].addEventListener("click", function (event) {
+  output.append(".");
+});
+
+// operators appending
+
+operatorsArray[0].addEventListener("click", function (event) {
+  output.append("+");
+});
+
+operatorsArray[1].addEventListener("click", function (event) {
+  output.append("-");
+});
+
+operatorsArray[2].addEventListener("click", function (event) {
+  output.append("x");
+});
+
+operatorsArray[3].addEventListener("click", function (event) {
+  output.append("/");
+});
+
+// mainBtns functionality
+
 mainBtns[2].addEventListener("click", function (event) {
-  alert(outputVal);
+  output.remove();
+  location.reload();
+});
+
+mainBtns[0].addEventListener("click", function (event) {
+  output.remove();
+  location.reload();
 });
